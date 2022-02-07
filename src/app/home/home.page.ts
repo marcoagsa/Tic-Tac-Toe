da555ade85a8067898a2d0ac95123d0be76b3f03
Marco Sá <marcoagsa@gmail.com>
2022-02-07 17:40:58 +0000

    ✨ homepage layout

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -7,6 +7,10 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
+  pageTitle = 'Rooster Game';
+  gameLogo = '/assets/roosterImg.jpeg';
+  startButtonLabel = 'start';
+
   constructor() {}
 
 }
