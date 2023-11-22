c2b041ec1e5440752607bdfdd11515aaaaa80dec
Marco Sa <marco.sa@md3.pt>
2023-11-22 20:59:44 +0000

    remove unnecessary declaration

--- src/app/components/shared-module.ts
+++ src/app/components/shared-module.ts
@@ -2,12 +2,11 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
-import { PopoverSelectIconComponent } from './popover-select-icon/popover-select-icon.component';
 import { TictactoeIconComponent } from '.';
 
 @NgModule({
   imports: [CommonModule, FormsModule, IonicModule, TictactoeIconComponent],
-  declarations: [PopoverSelectIconComponent],
-  exports: [PopoverSelectIconComponent, TictactoeIconComponent],
+  declarations: [],
+  exports: [TictactoeIconComponent],
 })
 export class SharedModule {}
