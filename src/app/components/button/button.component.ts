cf1526ec783a48fca760df5fb2dce9bdd5ab0f61
Marco Sa <marco.sa@md3.pt>
2023-11-23 10:49:04 +0000

    create standalone button

---
+++ src/app/components/button/button.component.ts
@@ -0,0 +1,27 @@
+import { TitleCasePipe } from '@angular/common';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { IonicModule } from '@ionic/angular';
+
+@Component({
+  selector: 'app-button',
+  standalone: true,
+  imports: [IonicModule, TitleCasePipe],
+  template: `
+    <ion-button
+      class="ion-margin"
+      expand="block"
+      color="tic-tac-toe"
+      shape="round"
+      size="large"
+      mode="ios"
+      (click)="triggerClick.emit()"
+    >
+      {{ label | titlecase }}
+    </ion-button>
+  `,
+  styles: ``,
+})
+export class ButtonComponent {
+  @Input({ required: true }) label: string;
+  @Output() triggerClick = new EventEmitter();
+}
