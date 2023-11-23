1abcda3654b9b78c62285b880794af627d20834f
Marco Sa <marco.sa@md3.pt>
2023-11-23 10:49:59 +0000

    declare components on index file

--- src/app/components/index.ts
+++ src/app/components/index.ts
@@ -1,2 +1,4 @@
 export * from './tictactoe-icon/tictactoe-icon.component';
 export * from './score-header/score-header.component';
+export * from './welcome/welcome.component';
+export * from './button/button.component';
