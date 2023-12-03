a24e1fb59130812cab07b583ef2a29dbb940f0ba
Marco Sa <marco.sa@md3.pt>
2023-12-03 15:29:26 +0000

    update appId

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
+  appId: 'com.msa.tic-tac-toe',
   appName: 'Tic-Tac-Toe',
   webDir: 'www',
 };
